0ad3d17493cb60e9c6d0215a22a6e4dca28af565
MAMUNYOZ <milangito@hotmail.com>
2020-10-23 15:28:59 +0200

    V0.4.1 - Ajustes Buscador

--- src/app/pages/search/search-breadcrumb/search-breadcrumb.component.ts
+++ src/app/pages/search/search-breadcrumb/search-breadcrumb.component.ts
@@ -5,22 +5,21 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-search-breadcrumb',
   templateUrl: './search-breadcrumb.component.html',
-  styleUrls: ['./search-breadcrumb.component.css']
+  styleUrls: ['./search-breadcrumb.component.css'],
 })
 export class SearchBreadcrumbComponent implements OnInit {
+  breadcrumb: string = null;
 
-	breadcrumb:string = null;
+  constructor(private activateRoute: ActivatedRoute) {}
 
- 	constructor(private activateRoute: ActivatedRoute) { }
-
-  	ngOnInit(): void {
-
-  		/*=============================================
+  ngOnInit(): void {
+    /*=============================================
 		Capturamos el parámetro URL
-		=============================================*/	
+		=============================================*/
 
-		this.breadcrumb = this.activateRoute.snapshot.params["param"].replace(/[_]/g, " ");
-
-  	}
-
+    this.breadcrumb = this.activateRoute.snapshot.params['param'].replace(
+      /[_]/g,
+      ' '
+    );
+  }
 }
